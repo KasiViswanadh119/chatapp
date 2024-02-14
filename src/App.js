6c48be6268a74ebdc2633329e330fd35a9408ab0
Kasi Viswanadh Duvva <21mh5a4916@acoe.edu.in>
2024-02-14 19:59:38 +0530

    ChatRoom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import ChatRoom from './components/ChatRoom';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/chat" component={ChatRoom} />
+        <Route path="/profile" component={UserProfile} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/ChatRoom.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { io } from "socket.io-client";
+
+const socket = io('<http://localhost:3000>');
+
+function ChatRoom() {
+  const [messages, setMessages] = useState([]);
+  const [messageText, setMessageText] = useState('');
+  const [user, setUser] = useState(null);
+
+  // Join a chat room
+  const joinChatRoom = (userDetails) => {
+    setUser(userDetails);
+    socket.emit('join', userDetails);
+  };
+
+  // Leave a chat room
+  const leaveChatRoom = () => {
+    socket.emit('leave', user);
+    setUser(null);
+  };
+
+  // More code will go here
+
+  return (
+    <div className="chat-room">
+      // The chat room components will go here
+    </div>
+  );
+}
+
+export default ChatRoom;
